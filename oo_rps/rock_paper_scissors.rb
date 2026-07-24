96087d5e2e676eb7ac00c929d29e8ffac1836a90
TacoMilitary <wlolams@outlook.com>
2026-07-24 00:27:51 -0500

    Refactoring of OO RPS, and Tournament style 10 wins bonus feature

--- oo_rps/rock_paper_scissors.rb
+++ oo_rps/rock_paper_scissors.rb
@@ -6,6 +6,22 @@ COMPUTER_IDENTITY = :computer
 COMPUTER_DISPLAY_NAME = 'Computer'.freeze
 DEFAULT_HUMAN_DISPLAY_NAME = 'Human'.freeze
 
+CLI_DIVIDER = "\n\n==================\n"
+
+def divide_screen
+  print CLI_DIVIDER
+end
+
+def clear_screen
+  system('clear') || system('cls') || divide_screen
+end
+
+def generic_prompt(message = 'Please input text:')
+  puts message
+  print '> '
+  gets.chomp.strip.downcase
+end
+
 class Player
   attr_reader :move
   attr_accessor :name
@@ -28,8 +44,8 @@ class Player
   def ask_name
     return nil unless human?
 
-    puts 'What do you want to be called?'
-    self.name = format_name(gets.chomp)
+    answer = generic_prompt('What do you want to be called?')
+    self.name = format_name(answer)
   end
 
   private
@@ -40,8 +56,8 @@ class Player
 
   def prompt_move
     loop do
-      puts 'Please choose rock, paper, or scissors:'
-      found_move = Move.find_move(gets.chomp.strip)
+      choice = generic_prompt('Please choose rock, paper, or scissors:')
+      found_move = Move.find_move(choice)
       return found_move if found_move
 
       puts 'Sorry. Invalid choice!'
@@ -59,11 +75,13 @@ end
 
 class Move
   VALID_MOVES = %w(rock paper scissors).map(&:freeze).freeze
+  # rubocop:disable Layout/FirstHashElementIndentation
   WINNING_MOVES = {
                     'rock' => 'scissors',
                     'paper' => 'rock',
                     'scissors' => 'paper'
                   }.freeze
+  # rubocop:enable Layout/FirstHashElementIndentation
 
   def self.random_move
     Move.new VALID_MOVES.sample
@@ -72,7 +90,7 @@ class Move
   def self.find_move(string)
     return nil if string.empty?
 
-    found_choice = VALID_MOVES.find do |choice| 
+    found_choice = VALID_MOVES.find do |choice|
       choice.start_with?(string) || string.start_with?(choice)
     end
 
@@ -101,36 +119,71 @@ class Move
 end
 
 class RPSGame
+  GRAND_WIN_SCORE = 10
   TIE_MESSAGE = "It's a tie!"
 
   def initialize
     @human = Player.new
     @computer = Player.new COMPUTER_IDENTITY
+    reset_scores
   end
 
+  # rubocop:disable Metrics/MethodLength
   def play
-    human.ask_name
-    display_welcome_message
+    clear_screen
+    intro_sequence
 
     loop do
-      human.choose
-      computer.choose
-      display_match_results
-      break unless play_again?
+      play_round
+      if grand_winner?
+        display_grand_winner
+        divide_screen
+        break unless play_again?
+        clear_screen
+        reset_scores
+      end
     end
 
+    clear_screen
     display_goodbye_message
   end
+  # rubocop:enable Metrics/MethodLength
 
   private
 
-  attr_reader :human, :computer
+  attr_reader :human, :computer, :human_score, :cpu_score
+
+  def reset_scores
+    @human_score = 0
+    @cpu_score = 0
+  end
+
+  def intro_sequence
+    human.ask_name
+
+    divide_screen
+    display_welcome_message
+  end
+
+  def play_round
+    display_scores
+    human.choose
+    computer.choose
+
+    clear_screen # divide_screen
+    display_match_results
+    increment_score
+
+    divide_screen
+  end
 
   def display_welcome_message
     puts "Hello #{human}! Welcome to Rock, Paper, Scissors!"
   end
 
   def display_goodbye_message
+    display_grand_winner
+    display_scores
     puts 'Goodbye! See you next time!'
   end
 
@@ -154,11 +207,38 @@ class RPSGame
     end
   end
 
+  def increment_score
+    human_move = human.move
+    cpu_move = computer.move
+
+    if human_move.beats?(cpu_move)
+      @human_score += 1
+    elsif cpu_move.beats?(human_move)
+      @cpu_score += 1
+    end
+  end
+
+  def display_scores
+    puts "#{computer}: #{cpu_score} || #{human}: #{human_score}"
+  end
+
+  def grand_winner?
+    human_score >= GRAND_WIN_SCORE || cpu_score >= GRAND_WIN_SCORE
+  end
+
+  def display_grand_winner
+    if human_score >= GRAND_WIN_SCORE
+      puts "#{human} wins this tournament!"
+    else
+      puts "#{computer} wins this tournament!"
+    end
+  end
+
   def play_again?
     loop do
-      puts "Would you like to play again? (y/n)"
-      answer = gets.chomp.strip.downcase
+      answer = generic_prompt('Would you like to play again? (y/n)')
       return answer == 'y' if ['y', 'n'].include?(answer)
+
       puts 'Sorry, must be y or n.'
     end
   end
