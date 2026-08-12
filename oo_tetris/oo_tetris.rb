76fb9c9b7df8e2a050a7eec5146b7ae896c7b320
Brandon <brandon@localhost.localdomain>
2026-08-12 00:00:00 -0500

    refined the grid display

--- oo_tetris/oo_tetris.rb
+++ oo_tetris/oo_tetris.rb
@@ -3,32 +3,60 @@ class GridSquare
   EMPTY_SQUARE_VISUAL = '-'
   FILLED_SQUARE_VISUAL = '[ ]'
 
+  attr_reader :filled
+
   def initialize
-    @visual = EMPTY_SQUARE_VISUAL
-    @visual = FILLED_SQUARE_VISUAL if rand(1..30) <= 2
+    clear
+    fill if rand(1..30) <= 2
   end
 
   def to_s
     visual.center SQUARE_SIZE
   end
 
+  def clear
+    self.filled = false
+    self.visual = EMPTY_SQUARE_VISUAL
+  end
+
+  def fill
+    self.filled = true
+    self.visual = FILLED_SQUARE_VISUAL
+  end
+
   private
 
-  attr_reader :visual
+  attr_writer :filled
+
+  attr_accessor :visual
 end
 
 class Grid
+  GRID_BORDER_VERT = '||'
+  GRID_BORDER_HORIZ = '='
+
   def initialize(width = 20, height = 35)
+    @board_visual_width = (width * GridSquare::SQUARE_SIZE) +
+                          (GRID_BORDER_VERT.size * 2)
     @coordinate = init_coordinate(width, height)
   end
 
   def to_s
-    coordinate.values.map do |row|
-      row.values.join
-    end.join "\n"
+    vertical_visual = grid_vertical_visual
+    "#{vertical_visual}\n#{grid_playable_visual}\n#{vertical_visual}"
   end
 
   private
+
+  def grid_vertical_visual
+    GRID_BORDER_HORIZ * board_visual_width
+  end
+
+  def grid_playable_visual
+    coordinate.values.map do |row|
+      "#{GRID_BORDER_VERT}#{row.values.join}#{GRID_BORDER_VERT}"
+    end.join "\n"
+  end
 
   def init_coordinate(width, height)
     coordinate = Hash.new
@@ -39,7 +67,7 @@ class Grid
     coordinate
   end
 
-  attr_reader :coordinate
+  attr_reader :coordinate, :board_visual_width
 end
 
 puts Grid.new
